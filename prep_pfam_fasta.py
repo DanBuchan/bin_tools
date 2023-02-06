46e84f98c453ec5db82604716f81911335fbd9a3
Daniel Buchan <clone@myself.com>
2023-02-06 15:41:01 +0000

    I think I've put the try in the right place

--- prep_pfam_fasta.py
+++ prep_pfam_fasta.py
@@ -15,31 +15,27 @@ family_count = 0
 read_count = 0
 with open(pfam_a_file, "r", encoding="utf-8", errors="replace") as fh:
     for line in fh:
-        try:
-            # if family_count == 2:
-            #     exit()
-            if line.startswith("//"):
-                if len(current_family) != 0:
-                    print(f"PRINTING: {pfam_family_id}")
-                    for line in current_family:
-                        entries = line.split()
-                        fhFasta.write(f">{entries[0]}|{pfam_family_id}\n")
-                        fhFasta.write(f"{entries[1].replace('.','').replace('-','').upper()}\n")
-                    random_rep = random.choice(current_family)
-                    entries = random_rep.split()
-                    fhReps.write(f">{entries[0]}|{pfam_family_id}\n")
-                    fhReps.write(f"{entries[1].replace('.','').replace('-','').upper()}\n")
-                    current_family = []
-                    family_count += 1
-                    continue
-            elif line.startswith("#=GF AC"):
-                pfam_family_id = line[10:].split(".")[0]
-                read_count += 1
-            elif not line.startswith("#"):
-                current_family.append(line.rstrip())
-        except Exception as e:
-            print(f"CAUGHT ONE: {e}")
-            continue
+        # if family_count == 2:
+        #     exit()
+        if line.startswith("//"):
+            if len(current_family) != 0:
+                print(f"PRINTING: {pfam_family_id}")
+                for line in current_family:
+                    entries = line.split()
+                    fhFasta.write(f">{entries[0]}|{pfam_family_id}\n")
+                    fhFasta.write(f"{entries[1].replace('.','').replace('-','').upper()}\n")
+                random_rep = random.choice(current_family)
+                entries = random_rep.split()
+                fhReps.write(f">{entries[0]}|{pfam_family_id}\n")
+                fhReps.write(f"{entries[1].replace('.','').replace('-','').upper()}\n")
+                current_family = []
+                family_count += 1
+                continue
+        elif line.startswith("#=GF AC"):
+            pfam_family_id = line[10:].split(".")[0]
+            read_count += 1
+        elif not line.startswith("#"):
+            current_family.append(line.rstrip())
 
 
 if len(current_family) != 0:
